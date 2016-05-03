2c895c28c5db7069c46b2eb0ce32ed5ded39852b
Gabriel Noal <gabriel.oliva@hotmail.com>
2016-05-03 08:42:14 -0300

    #

--- Tela_jogo.py
+++ Tela_jogo.py
@@ -18,10 +18,13 @@ FPS=30
 
 tela_largura=800
 tela_altura=600
-altura_carta_mao = tela_altura/4
-largura_carta_mao = tela_altura/7
-posição_y_carta_mao=tela_altura/2
-posição_x_carta_mao=tela_largura/2
+altura_carta = tela_altura/4
+largura_carta = tela_altura/7
+posição_y_carta=(tela_altura/2)-100
+posição_x_carta=tela_largura/2
+mover_carta_y=0
+mover_carta_x=0
+
 tela_truco = pygame.display.set_mode((tela_largura,tela_altura))
 pygame.display.set_caption('Truco')
 
@@ -33,13 +36,16 @@ while not fim_do_jogo:
         if event.type == pygame.QUIT:
             fim_do_jogo = True
             
-        if event.type == pygame.K_SPACE:
-            pass
             
-        
+        if event.type == pygame.K_SPACE:
+                mover_carta_y+=100
+            
+    posição_y_carta+= mover_carta_y
+    posição_x_carta+= mover_carta_x
+
         
     tela_truco.fill(verde)
-    tela_truco.fill(branco, rect=[posição_x_carta_mao,posição_y_carta_mao,altura_carta_mao,largura_carta_mao]) #Fill é mais usado por ser mais rapido
+    tela_truco.fill(branco, rect=[posição_x_carta,posição_y_carta,largura_carta,altura_carta]) #Fill é mais usado por ser mais rapido
     
     pygame.display.update()
     
