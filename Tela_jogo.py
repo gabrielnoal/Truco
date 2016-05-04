e8df01bff144e10630e4e5bf0d1b83369baa9340
Gabriel Noal <gabriel.oliva@hotmail.com>
2016-05-04 11:38:53 -0300

    3 Cartas e 1 baralho

--- Tela_jogo.py
+++ Tela_jogo.py
@@ -23,27 +23,37 @@ tela_altura=600
 tela_truco = pygame.display.set_mode((tela_largura,tela_altura))
 pygame.display.set_caption('Truco')
 
-costas_da_carta = pygame.image.load("back_card.png")
+#costas_da_carta = pygame.image.load("back_card.png")
+#costas_da_carta = pygame.transform.scale(costas_da_carta, (largura_carta,altura_carta))
 
-def carta_virada():
-    tela_truco.blit(costas_da_carta, [posição_x_baralho,posição_y_baralho])
 
 def loop_de_jogo():
     fim_do_app = False
     fim_do_jogo= False
-
+    costas_da_carta = pygame.image.load("back_card.png")
     posição_x_baralho=(tela_largura/2) 
-    posição_y_baralho=(tela_altura/2) -100
+    posição_y_baralho=(tela_altura/2) -50
 
     altura_carta = tela_altura/6 
     largura_carta = tela_altura/9
     
     posição_x_carta1=(tela_largura/2)
-    posição_y_carta1=(tela_altura/2)-100
+    posição_y_carta1=(tela_altura/2)-50
+    
     posição_x_carta2=(tela_largura/2)
-    posição_y_carta2=(tela_altura/2)-100
+    posição_y_carta2=(tela_altura/2)-50
+
     posição_x_carta3=(tela_largura/2)
-    posição_y_carta3=(tela_altura/2)-100
+    posição_y_carta3=(tela_altura/2)-50
+
+    posição_x_carta4=(tela_largura/2)
+    posição_y_carta4=(tela_altura/2)-50
+
+    posição_x_carta5=(tela_largura/2)
+    posição_y_carta5=(tela_altura/2)-50
+
+    posição_x_carta6=(tela_largura/2)
+    posição_y_carta6=(tela_altura/2)-50
     
     mover_carta_y=0
     mover_carta_x=0
@@ -65,24 +75,38 @@ def loop_de_jogo():
                         posição_y_carta1 = tela_altura- altura_carta
                         posição_y_carta2 = posição_y_carta1
                         posição_y_carta3 = posição_y_carta2
+
+                        posição_y_carta4=0
+                        posição_y_carta5=0
+                        posição_y_carta6=0
                         
                         posição_x_carta1 -= largura_carta 
                         posição_x_carta2 = posição_x_carta1 + largura_carta + 20
                         posição_x_carta3 = posição_x_carta2 + largura_carta + 20
+                        
+                        posição_x_carta4 -= largura_carta 
+                        posição_x_carta5 = posição_x_carta4 + largura_carta + 20
+                        posição_x_carta6 = posição_x_carta5 + largura_carta + 20
+                        
                         
                         
                 
 #        posição_y_carta+= mover_carta_y
 #        posição_x_carta+= mover_carta_x
     
-            
+        costas_da_carta = pygame.transform.scale(costas_da_carta, (int(largura_carta),int(altura_carta)))
+ 
         tela_truco.fill(verde)
-        tela_truco.blit(costas_da_carta, [posição_x_baralho,posição_y_baralho, largura_carta,altura_carta])
         tela_truco.fill(branco, rect=[posição_x_carta1,posição_y_carta1,largura_carta,altura_carta]) #Fill é mais usado por ser mais rapido
         tela_truco.fill(branco, rect=[posição_x_carta2,posição_y_carta2,largura_carta,altura_carta]) #Fill é mais usado por ser mais rapido
         tela_truco.fill(branco, rect=[posição_x_carta3,posição_y_carta3,largura_carta,altura_carta]) #Fill é mais usado por ser mais rapido
         
-        tela_truco.fill(branco, rect=[posição_x_baralho,posição_y_baralho,largura_carta,altura_carta])
+        tela_truco.fill(branco, rect=[posição_x_carta4,posição_y_carta4,largura_carta,altura_carta]) #Fill é mais usado por ser mais rapido
+        tela_truco.fill(branco, rect=[posição_x_carta5,posição_y_carta5,largura_carta,altura_carta]) #Fill é mais usado por ser mais rapido
+        tela_truco.fill(branco, rect=[posição_x_carta6,posição_y_carta6,largura_carta,altura_carta]) #Fill é mais usado por ser mais rapido
+
+        tela_truco.blit(costas_da_carta, [posição_x_baralho,posição_y_baralho])
+
         pygame.display.update()
     
 
