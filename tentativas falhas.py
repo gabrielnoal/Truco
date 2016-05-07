66ad5914bf09d17186a65f7d415ddeb2265dc808
Gabriel Noal <gabriel.oliva@hotmail.com>
2016-05-07 16:57:08 -0300

    falta só arrumar a função joga_carta q ta bugada

--- tentativas falhas.py
+++ tentativas falhas.py
@@ -5,7 +5,7 @@ Created on Fri May  6 14:24:44 2016
 @author: Usuario
 """
 import random
-class Truco:
+class Logica:
     def __init__(self):
         self.baralho = ['Quatro_ouros','Quatro_espadas','Quatro_copas','Quatro_paus',
                    'Cinco_ouros','Cinco_espadas','Cinco_copas','Cinco_paus',
@@ -40,11 +40,19 @@ class Truco:
         self.a = self.vira_carta.split('_')
         self.b = self.a[1]
         #SE a vira for 3 fiz esse código para não dar erro
-        if self.vira in (36,37,38,39):
+        if self.vira in [36,37,38,39]:
             self.pika = self.baralho[0]
             self.espadilha = self.baralho[1]
             self.copa = self.baralho[2]
             self.zap = self.baralho[3]
+            
+            del self.baralho[self.vira]
+            self.dic_baralho[self.pika]=11
+            self.dic_baralho[self.espadilha]=12
+            self.dic_baralho[self.copa]=13
+            self.dic_baralho[self.zap]=14
+            
+            return self.dic_baralho, self.baralho, [self.vira]
         else:
             if self.b == 'ouros':
                 self.pika = self.baralho[self.vira+4]
@@ -76,112 +84,115 @@ class Truco:
             self.dic_baralho[self.copa]=13
             self.dic_baralho[self.zap]=14
             
-            return self.dic_baralho, self.baralho,[self.vira,self.baralho[self.vira]]
+            return self.dic_baralho, self.baralho, [self.vira]
             
-        def distribuir_cartas(self):
-            self.carta_1=random.randint(0,len(self.baralho)-1)
-            self.mão_jogador_0.append(self.carta_1)
-            del self.baralho[self.carta_1]
+    def distribuir_cartas(self):
+        self.carta_1=random.randint(0,39-1)
+        self.mão_jogador_0.append(self.baralho[self.carta_1])
+        del self.baralho[self.carta_1]
             
-            self.carta_2=random.randint(0,len(self.baralho)-1)
-            self.mão_jogador_0.append(self.carta_2)
-            del self.baralho[self.carta_2]
+        self.carta_2=random.randint(0,39-2)
+        self.mão_jogador_0.append(self.baralho[self.carta_2])
+        del self.baralho[self.carta_2]
             
-            self.carta_3=random.randint(0,len(self.baralho)-1)
-            self.mão_jogador_0.append(self.carta_3)
-            del self.baralho[self.carta_3]
+        self.carta_3=random.randint(0,39-3)
+        self.mão_jogador_0.append(self.baralho[self.carta_3])
+        del self.baralho[self.carta_3]
             
-            self.carta_4=random.randint(0,len(self.baralho)-1)
-            self.mão_jogador_1.append(self.carta_4)
-            del self.baralho[self.carta_4]
+        self.carta_4=random.randint(0,39-4)
+        self.mão_jogador_1.append(self.baralho[self.carta_4])
+        del self.baralho[self.carta_4]
             
-            self.carta_5=random.randint(0,len(self.baralho)-1)
-            self.mão_jogador_1.append(self.carta_5)
-            del self.baralho[self.carta_5]
+        self.carta_5=random.randint(0,39-5)
+        self.mão_jogador_1.append(self.baralho[self.carta_5])
+        del self.baralho[self.carta_5]
             
-            self.carta_6=random.randint(0,len(self.baralho)-1)
-            self.mão_jogador_1.append(self.carta_6)
-            del self.baralho[self.carta_6]
-            
-            print(self.mão_jogador0)
-            print(self.mão_jogador1)
-            return self.mão_jogador0,self.mão_jogador1
-            
-        def troca_jogador(self):
-            if self.jogador == 0:
-                self.jogador =1
-            else:
-                self.jogador =0
-            return self.jogador
+        self.carta_6=random.randint(0,39-6)
+        self.mão_jogador_1.append(self.baralho[self.carta_6])
+        del self.baralho[self.carta_6]
+        
+#        print (self.baralho)
 
-        def joga_carta(self):                
-            while self.jogador ==0:
-                self.pergunta= input('Jogador 1: Qual carta quer jogar?')
-                if self.pegunta==1:
-                    if self.mão_jogador0[0]==-1:
-                        print("Essa carta ja foi jogada, tentei jogar outra carta")
-                    else:
-                        self.mesa.append(self.mão_jogador0[0])
-                        self.mão_jogador0[0]=-1
-                        troca_jogador()
-                        return self.mesa
-                elif self.pergunta==2:
-                    if self.mão_jogador0[1]==-1:
-                        print("Essa carta ja foi jogada, tentei jogar outra carta")
-                    else:
-                        self.mesa.append(self.mão_jogador0[1])
-                        self.mão_jogador0[1]=-1
-                        troca_jogador()
-                        return self.mesa
-                elif self.pergunta==3:
-                    if self.mão_jogador0[2]==-1:
-                        print("Essa carta ja foi jogada, tentei jogar outra carta")
-                    else:
-                        self.mesa.append(self.mão_jogador0[2])
-                        self.mão_jogador0[2]=-1
-                        troca_jogador()
-                        return self.mesa
+        return self.mão_jogador_0,self.mão_jogador_1
             
-            while self.jogador ==1:
-                self.pergunta= input('Jogador 2: Qual carta quer jogar?')
-                if self.pegunta==1:
-                    if self.mão_jogador1[0]==-1:
-                        print("Essa carta ja foi jogada, tentei jogar outra carta")
-                    else:
-                        self.mesa.append(self.mão_jogador1[0])
-                        self.mão_jogador1[0]=-1
-                        troca_jogador()
-                        return self.mesa
-                elif self.pergunta==2:
-                    if self.mão_jogador1[1]==-1:
-                        print("Essa carta ja foi jogada, tentei jogar outra carta")
-                    else:
-                        self.mesa.append(self.mão_jogador1[1])
-                        self.mão_jogador1[1]=-1
-                        troca_jogador()
-                        return self.mesa
-                elif self.pergunta==3:
-                    if self.mão_jogador1[2]==-1:
-                        print("Essa carta ja foi jogada, tentei jogar outra carta")
-                    else:
-                        self.mesa.append(self.mão_jogador1[2])
-                        self.mão_jogador1[2]=-1
-                        troca_jogador()
-                        return self.mesa
-                
-        def limpa_jogadas(self):
-            while len(self.mesa)<2:
-                print(self.mesa)
-                continue
+    def troca_jogador(self):
+        if self.jogador == 0:
+            self.jogador =1
+        else:
+            self.jogador =0
+        return self.jogador
+            
+    def joga_carta(self):                
+        while self.jogador ==0:
+            self.pergunta= input('Jogador 1: Qual carta quer jogar?')
+            if self.pergunta==1:
+                if self.mão_jogador0[0]==-1:
+                    print("Essa carta ja foi jogada, tente jogar outra carta")
+                else:
+                    self.mesa.append(self.mão_jogador0[0])
+                    self.mão_jogador0[0]=-1
+                    troca_jogador()
+                    return self.mesa
+            elif self.pergunta==2:
+                if self.mão_jogador0[1]==-1:
+                    print("Essa carta ja foi jogada, tente jogar outra carta")
+                else:
+                    self.mesa.append(self.mão_jogador0[1])
+                    self.mão_jogador0[1]=-1
+                    jogo.troca_jogador()
+                    return self.mesa
+            elif self.pergunta==3:
+                if self.mão_jogador0[2]==-1:
+                    print("Essa carta ja foi jogada, tente jogar outra carta")
+                else:
+                    self.mesa.append(self.mão_jogador0[2])
+                    self.mão_jogador0[2]=-1
+                    jogo.troca_jogador()
+                    return self.mesa
+        
+        while self.jogador ==1:
+            self.pergunta= input('Jogador 2: Qual carta quer jogar?')
+            if self.pegunta==1:
+                if self.mão_jogador1[0]==-1:
+                    print("Essa carta ja foi jogada, tente jogar outra carta")
+                else:
+                    self.mesa.append(self.mão_jogador1[0])
+                    self.mão_jogador1[0]=-1
+                    jogo.troca_jogador()
+                    return self.mesa
+            elif self.pergunta==2:
+                if self.mão_jogador1[1]==-1:
+                    print("Essa carta ja foi jogada, tente jogar outra carta")
+                else:
+                    self.mesa.append(self.mão_jogador1[1])
+                    self.mão_jogador1[1]=-1
+                    jogo.troca_jogador()
+                    return self.mesa
+            elif self.pergunta==3:
+                if self.mão_jogador1[2]==-1:
+                    print("Essa carta ja foi jogada, tente jogar outra carta")
+                else:
+                    self.mesa.append(self.mão_jogador1[2])
+                    self.mão_jogador1[2]=-1
+                    jogo.troca_jogador()
+                    return self.mesa
+        
+    def limpa_jogadas(self):
+        while len(self.mesa)<2:
             print(self.mesa)
-            del self.mesa[0]
-            del self.mesa[1]
-            return self.mesa
-            
-        def verifica_ganhador(self):
+            continue
+        print(self.mesa)
+        del self.mesa[0]
+        del self.mesa[1]
+        return self.mesa
             
                 
                 
-truco=Truco()
-print(truco.manilha())
+jogo=Logica()
+print(jogo.manilha())
 
+
+print(jogo.distribuir_cartas())
+
+
+print(jogo.joga_carta())
